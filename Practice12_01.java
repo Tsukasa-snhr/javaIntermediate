65c034ee45cb25a467258947a9b6cd35d8b4fbc8
Tsukasa-snhr <tsukasasandesu@gmail.com>
2025-04-13 15:44:36 +0900

    Add files via upload

---
+++ Practice12_01.java
@@ -0,0 +1,8 @@
+public class Practice12_01 {
+    public static void main(String[] args) {
+
+        int x = 2;
+        int y = 3;
+        System.out.println("二つの引数の減算は" + (x - y));
+    }
+}
---
+++ Practice12_02.java
@@ -0,0 +1,16 @@
+public class Practice12_02 {
+
+        public static void main(String[] args) {
+            String time = "昼" ;
+           
+
+            if (time.equals("朝")) {
+                System.out.println("Good morning!!");
+            } else if (time.equals("昼")) {
+                System.out.println("Good afternoon!!");
+            } else if (time.equals("夜")) {
+                System.out.println("Good evening!!");
+            }
+        
+        }
+    }
